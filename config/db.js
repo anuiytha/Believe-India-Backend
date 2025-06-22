a66d9965b692476d2388fb35dc8ebab8fde6e36c
221910310008 <221910310008@gitam.in>
2025-06-22 17:18:15 -0400

    Allowed SSL

--- config/db.js
+++ config/db.js
@@ -4,6 +4,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // This skips cert validation for easier setup
+        }
+    }
 });
 
 module.exports = sequelize;
